9d45e802a27764ed89d2bc9068b428f769a0f916
roxxuv-dev <rox.xuv@gmail.com>
2026-05-31 19:54:56 +0300

    Create src/client/java/counting/stepcounter/client/StepCounterClient.java

---
+++ src/client/java/counting/stepcounter/client/StepCounterClient.java
@@ -0,0 +1,10 @@
+package counting.stepcounter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StepCounterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
