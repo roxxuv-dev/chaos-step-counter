eaa4cc7fd2b989485cd892bb12611d7764e22455
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:36:48 +0300

    Update SpeedBoostEvent.java

--- src/main/java/counting/stepcounter/event/events/SpeedBoostEvent.java
+++ src/main/java/counting/stepcounter/event/events/SpeedBoostEvent.java
@@ -1,1 +1,26 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class SpeedBoostEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Speed Boost";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.addEffect(
+                new MobEffectInstance(
+                        MobEffects.MOVEMENT_SPEED,
+                        300,
+                        2
+                )
+        );
+    }
+}
