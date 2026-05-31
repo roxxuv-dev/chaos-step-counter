9cd62576ab848d26256be675cc49d52f3ba3a0fd
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:33:06 +0300

    Update LightningEvent.java

--- src/main/java/counting/stepcounter/event/events/LightningEvent.java
+++ src/main/java/counting/stepcounter/event/events/LightningEvent.java
@@ -1,1 +1,39 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.EntitySpawnReason;
+
+public class LightningEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Lightning Strike";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        ServerLevel level = player.serverLevel();
+
+        LightningBolt bolt =
+                EntityType.LIGHTNING_BOLT.create(
+                        level,
+                        EntitySpawnReason.MOB_SUMMONED
+                );
+
+        if (bolt != null) {
+
+            bolt.setPos(
+                    player.getX(),
+                    player.getY(),
+                    player.getZ()
+            );
+
+            level.addFreshEntity(bolt);
+        }
+    }
+}
