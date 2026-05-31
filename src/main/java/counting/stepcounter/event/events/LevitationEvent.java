5348b71ae3318a3f2970526f1657b0028319a04e
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:37:15 +0300

    Update LevitationEvent.java

--- src/main/java/counting/stepcounter/event/events/LevitationEvent.java
+++ src/main/java/counting/stepcounter/event/events/LevitationEvent.java
@@ -1,1 +1,26 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class LevitationEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Levitation";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.addEffect(
+                new MobEffectInstance(
+                        MobEffects.LEVITATION,
+                        80,
+                        2
+                )
+        );
+    }
+}
