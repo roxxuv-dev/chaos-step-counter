7b922c0b5ed3fdba40c838547eca1f2befb21d29
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:41:06 +0300

    Create PlayerJoinMixin.java

---
+++ src/main/java/counting/stepcounter/mixin/PlayerJoinMixin.java
@@ -0,0 +1,10 @@
+package counting.stepcounter.mixin;
+
+import counting.stepcounter.tutorial.TutorialBook;
+import net.minecraft.server.level.ServerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ServerPlayer.class)
+public class PlayerJoinMixin {
+
+}
