33ec00af607a313e759559f9e87d4e200eab3c16
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:36:34 +0300

    Update IronGolemEvent.java

--- src/main/java/counting/stepcounter/event/events/IronGolemEvent.java
+++ src/main/java/counting/stepcounter/event/events/IronGolemEvent.java
@@ -1,1 +1,39 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntitySpawnReason;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.IronGolem;
+
+public class IronGolemEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Iron Golem";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        ServerLevel level = player.serverLevel();
+
+        IronGolem golem =
+                EntityType.IRON_GOLEM.create(
+                        level,
+                        EntitySpawnReason.MOB_SUMMONED
+                );
+
+        if (golem != null) {
+
+            golem.setPos(
+                    player.getX() + 3,
+                    player.getY(),
+                    player.getZ()
+            );
+
+            level.addFreshEntity(golem);
+        }
+    }
+}
