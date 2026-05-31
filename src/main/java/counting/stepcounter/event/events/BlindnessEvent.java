c02729af7c7814af0670951ed320e655e7deacbd
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:34:31 +0300

    Update BlindnessEvent.java

--- src/main/java/counting/stepcounter/event/events/BlindnessEvent.java
+++ src/main/java/counting/stepcounter/event/events/BlindnessEvent.java
@@ -1,1 +1,26 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class BlindnessEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Blindness";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.addEffect(
+                new MobEffectInstance(
+                        MobEffects.BLINDNESS,
+                        200,
+                        0
+                )
+        );
+    }
+}
