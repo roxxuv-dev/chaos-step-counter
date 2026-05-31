f10c855c7fb1aa6bdfaf4b696754954f2c28f8ba
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:37:00 +0300

    Update TntEvent.java

--- src/main/java/counting/stepcounter/event/events/TntEvent.java
+++ src/main/java/counting/stepcounter/event/events/TntEvent.java
@@ -1,1 +1,35 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.PrimedTnt;
+
+public class TntEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "TNT";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        ServerLevel level = player.serverLevel();
+
+        PrimedTnt tnt =
+                EntityType.TNT.create(level);
+
+        if (tnt != null) {
+
+            tnt.setPos(
+                    player.getX() + 4,
+                    player.getY(),
+                    player.getZ()
+            );
+
+            level.addFreshEntity(tnt);
+        }
+    }
+}
