56896a88462a4f13c8d188a839326f5bcd5655a1
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:34:17 +0300

    Update LaunchEvent.java

--- src/main/java/counting/stepcounter/event/events/LaunchEvent.java
+++ src/main/java/counting/stepcounter/event/events/LaunchEvent.java
@@ -1,1 +1,24 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+
+public class LaunchEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Sky Launch";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.setDeltaMovement(
+                0,
+                2.2,
+                0
+        );
+
+        player.hurtMarked = true;
+    }
+}
