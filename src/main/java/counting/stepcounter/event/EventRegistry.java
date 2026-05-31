b2b2635647225feaf8c998a5e1f2720c1da5990b
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:29:40 +0300

    Update EventRegistry.java

--- src/main/java/counting/stepcounter/event/EventRegistry.java
+++ src/main/java/counting/stepcounter/event/EventRegistry.java
@@ -1,1 +1,22 @@
-.
+package counting.stepcounter.event;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+
+public class EventManager {
+
+    public static void triggerRandomEvent(ServerPlayer player) {
+
+        ChaosEvent event = EventRegistry.getRandomEvent();
+
+        if (event == null) {
+            return;
+        }
+
+        player.sendSystemMessage(
+                Component.literal("§c§lCHAOS EVENT §7» §f" + event.getName())
+        );
+
+        event.execute(player);
+    }
+}
