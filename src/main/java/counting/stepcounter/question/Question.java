a2b65cf5dd236b3f8992addb1ab05c96112b7651
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:37:58 +0300

    Update Question.java

--- src/main/java/counting/stepcounter/question/Question.java
+++ src/main/java/counting/stepcounter/question/Question.java
@@ -1,1 +1,32 @@
-.
+package counting.stepcounter.question;
+
+import java.util.List;
+
+public class Question {
+
+    private final String question;
+    private final List<String> answers;
+    private final int correctIndex;
+
+    public Question(
+            String question,
+            List<String> answers,
+            int correctIndex
+    ) {
+        this.question = question;
+        this.answers = answers;
+        this.correctIndex = correctIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public int getCorrectIndex() {
+        return correctIndex;
+    }
+}
