84ca42d3d8096e0a0df217fd030f5c509500adce
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:10:49 +0300

    Update ChaosEvent.java

--- src/main/java/counting/stepcounter/event/ChaosEvent.java
+++ src/main/java/counting/stepcounter/event/ChaosEvent.java
@@ -1,1 +1,10 @@
-.
+package counting.stepcounter.event;
+
+import net.minecraft.server.level.ServerPlayer;
+
+public interface ChaosEvent {
+
+    String getName();
+
+    void execute(ServerPlayer player);
+}
