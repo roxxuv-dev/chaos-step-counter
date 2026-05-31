52d78772d69654977f2d67755704cd64f262f8ea
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:35:11 +0300

    Update RandomTeleportEvent.java

--- src/main/java/counting/stepcounter/event/events/RandomTeleportEvent.java
+++ src/main/java/counting/stepcounter/event/events/RandomTeleportEvent.java
@@ -1,1 +1,28 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+
+public class RandomTeleportEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Random Teleport";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        double x =
+                player.getX() + (Math.random() * 40 - 20);
+
+        double z =
+                player.getZ() + (Math.random() * 40 - 20);
+
+        player.teleportTo(
+                x,
+                player.getY(),
+                z
+        );
+    }
+}
