6983f93ac9a341cc2ac04d6a23e1ec514dfbbdd5
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:35:28 +0300

    Update FreezeEvent.java

--- src/main/java/counting/stepcounter/event/events/FreezeEvent.java
+++ src/main/java/counting/stepcounter/event/events/FreezeEvent.java
@@ -1,1 +1,26 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class FreezeEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Frozen";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.addEffect(
+                new MobEffectInstance(
+                        MobEffects.MOVEMENT_SLOWDOWN,
+                        120,
+                        10
+                )
+        );
+    }
+}
