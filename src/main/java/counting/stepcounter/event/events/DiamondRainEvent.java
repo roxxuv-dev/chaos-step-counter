a790a0c53eef899381d85873597986ced26f633b
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:37:32 +0300

    Update DiamondRainEvent.java

--- src/main/java/counting/stepcounter/event/events/DiamondRainEvent.java
+++ src/main/java/counting/stepcounter/event/events/DiamondRainEvent.java
@@ -1,1 +1,39 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class DiamondRainEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Diamond Reward";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        ServerLevel level = player.serverLevel();
+
+        for (int i = 0; i < 3; i++) {
+
+            ItemEntity diamond =
+                    new ItemEntity(
+                            level,
+                            player.getX(),
+                            player.getY() + 5,
+                            player.getZ(),
+                            new ItemStack(
+                                    Items.DIAMOND,
+                                    1
+                            )
+                    );
+
+            level.addFreshEntity(diamond);
+        }
+    }
+}
