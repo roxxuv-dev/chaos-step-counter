7ee71a8fb2bc5baad338b7a88e6c002e6e7943a2
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:35:49 +0300

    Update DropItemEvent.java

--- src/main/java/counting/stepcounter/event/events/DropItemEvent.java
+++ src/main/java/counting/stepcounter/event/events/DropItemEvent.java
@@ -1,1 +1,21 @@
-.
+package counting.stepcounter.event.events;
+
+import counting.stepcounter.event.ChaosEvent;
+import net.minecraft.server.level.ServerPlayer;
+
+public class DropItemEvent implements ChaosEvent {
+
+    @Override
+    public String getName() {
+        return "Drop Item";
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+
+        player.drop(
+                player.getMainHandItem(),
+                true
+        );
+    }
+}
