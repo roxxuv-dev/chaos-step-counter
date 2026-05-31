592d170b1115b880c75b7ae9b2f4641f9466b8e2
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:41:47 +0300

    Update HudData.java

--- src/main/java/counting/stepcounter/hud/HudData.java
+++ src/main/java/counting/stepcounter/hud/HudData.java
@@ -1,1 +1,10 @@
-.
+package counting.stepcounter.hud;
+
+public class HudData {
+
+    public static long steps = 0;
+
+    public static long nextEventAt = 50;
+
+    public static boolean questionActive = false;
+}
