87219231ab988a068012e355dcfe4b88d8b6481c
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:10:25 +0300

    Update PlayerData.java

--- src/main/java/counting/stepcounter/player/PlayerData.java
+++ src/main/java/counting/stepcounter/player/PlayerData.java
@@ -1,1 +1,28 @@
-.
+package counting.stepcounter.player;
+
+public class PlayerData {
+
+    private long steps;
+    private long nextQuestionAt;
+
+    public PlayerData() {
+        this.steps = 0;
+        this.nextQuestionAt = 300;
+    }
+
+    public long getSteps() {
+        return steps;
+    }
+
+    public void addStep() {
+        steps++;
+    }
+
+    public long getNextQuestionAt() {
+        return nextQuestionAt;
+    }
+
+    public void setNextQuestionAt(long nextQuestionAt) {
+        this.nextQuestionAt = nextQuestionAt;
+    }
+}
