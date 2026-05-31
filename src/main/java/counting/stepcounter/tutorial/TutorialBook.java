18f14c909400aa139f54bff6b1d6212e1fdcb0c8
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 02:11:45 +0300

    Update TutorialBook.java

--- src/main/java/counting/stepcounter/tutorial/TutorialBook.java
+++ src/main/java/counting/stepcounter/tutorial/TutorialBook.java
@@ -1,1 +1,24 @@
-.
+package counting.stepcounter.tutorial;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.WrittenBookContent;
+
+import java.util.List;
+
+public class TutorialBook {
+
+    public static void give(ServerPlayer player) {
+
+        ItemStack book = new ItemStack(
+                Items.WRITTEN_BOOK
+        );
+
+        player.getInventory().setItem(
+                4,
+                book
+        );
+    }
+}
