5d6dbcc377277780de026a4c28c0ae630a5aa4ed
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 19:30:59 +0300

    Update ClientPacketHandler.java

--- src/client/java/counting/stepcounter/client/network/ClientPacketHandler.java
+++ src/client/java/counting/stepcounter/client/network/ClientPacketHandler.java
@@ -1,41 +1,9 @@
 package counting.stepcounter.client.network;
 
-import counting.stepcounter.client.screen.QuestionScreen;
-import counting.stepcounter.network.OpenQuestionPayload;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.Minecraft;
-
 public class ClientPacketHandler {
 
     public static void register() {
 
-        ClientPlayNetworking.registerGlobalReceiver(
-                OpenQuestionPayload.TYPE,
-                (payload, context) -> {
-
-                    Minecraft client =
-                            Minecraft.getInstance();
-
-                    client.execute(() ->
-
-                            client.setScreen(
-
-                                    new QuestionScreen(
-
-                                            payload.question(),
-
-                                            payload.answer1(),
-                                            payload.answer2(),
-                                            payload.answer3(),
-                                            payload.answer4(),
-
-                                            payload.correctAnswer(),
-
-                                            payload.expireTime()
-                                    )
-                            )
-                    );
-                }
-        );
+        // Networking not implemented yet.
     }
 }
