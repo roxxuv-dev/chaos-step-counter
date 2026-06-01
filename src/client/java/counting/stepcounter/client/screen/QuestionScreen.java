0c43035882173a821639f75271fad9fe14372ef1
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:34:21 +0300

    Update QuestionScreen.java

--- src/client/java/counting/stepcounter/client/screen/QuestionScreen.java
+++ src/client/java/counting/stepcounter/client/screen/QuestionScreen.java
@@ -9,52 +9,165 @@ public class QuestionScreen extends Screen {
 
     private final String question;
 
-    private final String[] answers;
+    private final String answer1;
+    private final String answer2;
+    private final String answer3;
+    private final String answer4;
+
+    private final int correctAnswer;
+
+    private final long expireTime;
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     public QuestionScreen(
             String question,
-            String[] answers
+            String answer1,
+            String answer2,
+            String answer3,
+            String answer4,
+            int correctAnswer,
+            long expireTime
     ) {
-        super(Component.literal("Chaos Question"));
+
+        super(Component.literal("Question"));
 
         this.question = question;
-        this.answers = answers;
+
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+
+        this.correctAnswer = correctAnswer;
+
+        this.expireTime = expireTime;
     }
 
     @Override
     protected void init() {
 
         int centerX = width / 2;
+        int centerY = height / 2;
 
-        int y = height / 2 - 20;
+        button1 =
+                Button.builder(
+                        Component.literal(answer1),
+                        button -> answerClicked(0)
+                )
+                .bounds(
+                        centerX - 100,
+                        centerY - 20,
+                        200,
+                        20
+                )
+                .build();
 
-        for (int i = 0; i < answers.length; i++) {
+        button2 =
+                Button.builder(
+                        Component.literal(answer2),
+                        button -> answerClicked(1)
+                )
+                .bounds(
+                        centerX - 100,
+                        centerY + 5,
+                        200,
+                        20
+                )
+                .build();
 
-            final int answerIndex = i;
+        button3 =
+                Button.builder(
+                        Component.literal(answer3),
+                        button -> answerClicked(2)
+                )
+                .bounds(
+                        centerX - 100,
+                        centerY + 30,
+                        200,
+                        20
+                )
+                .build();
 
-            addRenderableWidget(
-                    Button.builder(
-                                    Component.literal(
-                                            answers[i]
-                                    ),
-                                    button -> {
+        button4 =
+                Button.builder(
+                        Component.literal(answer4),
+                        button -> answerClicked(3)
+                )
+                .bounds(
+                        centerX - 100,
+                        centerY + 55,
+                        200,
+                        20
+                )
+                .build();
 
-                                        // TODO:
-                                        // send answer packet
-                                        // close screen
+        addRenderableWidget(button1);
+        addRenderableWidget(button2);
+        addRenderableWidget(button3);
+        addRenderableWidget(button4);
+    }
 
-                                        this.onClose();
-                                    }
-                            )
-                            .bounds(
-                                    centerX - 100,
-                                    y + (i * 25),
-                                    200,
-                                    20
-                            )
-                            .build()
+    private void answerClicked(
+            int selectedAnswer
+    ) {
+
+        if (selectedAnswer == correctAnswer) {
+
+            if (minecraft != null) {
+
+                minecraft.setScreen(null);
+            }
+
+        } else {
+
+            failQuestion();
+        }
+    }
+
+    private void failQuestion() {
+
+        if (minecraft != null &&
+                minecraft.player != null) {
+
+            minecraft.player.connection.sendCommand(
+                    "kill"
             );
         }
+
+        if (minecraft != null) {
+
+            minecraft.setScreen(null);
+        }
+    }
+
+    @Override
+    public void tick() {
+
+        super.tick();
+
+        long remaining =
+                expireTime -
+                        System.currentTimeMillis();
+
+        if (remaining <= 0) {
+
+            failQuestion();
+        }
+    }
+
+    @Override
+    public boolean shouldCloseOnEsc() {
+
+        return false;
+    }
+
+    @Override
+    public void onClose() {
+
     }
 
     @Override
@@ -62,34 +175,66 @@ public class QuestionScreen extends Screen {
             GuiGraphics graphics,
             int mouseX,
             int mouseY,
-            float delta
+            float partialTick
     ) {
 
         renderBackground(
                 graphics,
                 mouseX,
                 mouseY,
-                delta
-        );
-
-        graphics.drawCenteredString(
-                font,
-                question,
-                width / 2,
-                height / 2 - 60,
-                0xFFFFFF
+                partialTick
         );
 
         super.render(
                 graphics,
                 mouseX,
                 mouseY,
-                delta
+                partialTick
+        );
+
+        int centerX =
+                width / 2;
+
+        int centerY =
+                height / 2;
+
+        graphics.drawCenteredString(
+                font,
+                "QUESTION TIME",
+                centerX,
+                centerY - 80,
+                0xFF5555
+        );
+
+        graphics.drawCenteredString(
+                font,
+                question,
+                centerX,
+                centerY - 55,
+                0xFFFFFF
+        );
+
+        long secondsLeft =
+                Math.max(
+                        0,
+                        (expireTime
+                                - System.currentTimeMillis())
+                                / 1000
+                );
+
+        graphics.drawCenteredString(
+                font,
+                "Time Left: "
+                        + secondsLeft,
+                centerX,
+                centerY + 90,
+                0xFFFF55
         );
     }
 
     @Override
-    public boolean shouldCloseOnEsc() {
+    public boolean isPauseScreen() {
+
         return false;
     }
 }
