bb3bc044149a856bd5eca19e019d4840b17426b0
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:28:31 +0300

    Update PlayerJoinMixin.java

--- src/main/java/counting/stepcounter/mixin/PlayerJoinMixin.java
+++ src/main/java/counting/stepcounter/mixin/PlayerJoinMixin.java
@@ -3,8 +3,28 @@ package counting.stepcounter.mixin;
 import counting.stepcounter.tutorial.TutorialBook;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public class PlayerJoinMixin {
 
+    @Inject(
+            method = "restoreFrom",
+            at = @At("TAIL")
+    )
+    private void giveBook(
+            ServerPlayer oldPlayer,
+            boolean alive,
+            CallbackInfo ci
+    ) {
+
+        ServerPlayer player =
+                (ServerPlayer)(Object)this;
+
+        TutorialBook.giveTutorialBook(
+                player
+        );
+    }
 }
