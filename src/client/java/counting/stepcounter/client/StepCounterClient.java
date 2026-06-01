d54d7bcd3e2cf29572c9c09abcaa8595436f2f23
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:32:15 +0300

    Update StepCounterClient.java

--- src/client/java/counting/stepcounter/client/StepCounterClient.java
+++ src/client/java/counting/stepcounter/client/StepCounterClient.java
@@ -1,6 +1,7 @@
 package counting.stepcounter.client;
 
 import counting.stepcounter.client.hud.ChaosHudRenderer;
+import counting.stepcounter.client.network.ClientPacketHandler;
 import net.fabricmc.api.ClientModInitializer;
 
 public class StepCounterClient
@@ -10,5 +11,7 @@ public class StepCounterClient
     public void onInitializeClient() {
 
         ChaosHudRenderer.register();
+
+        ClientPacketHandler.register();
     }
 }
