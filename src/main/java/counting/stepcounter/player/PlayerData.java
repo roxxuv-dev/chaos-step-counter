5bcf7fa6ea4c49ab0b67880208028299fd3c6928
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 03:08:45 +0300

    Update PlayerData.java

--- src/main/java/counting/stepcounter/player/PlayerData.java
+++ src/main/java/counting/stepcounter/player/PlayerData.java
@@ -1,8 +1,16 @@
 package counting.stepcounter.player;
 
+import counting.stepcounter.question.Question;
+
 public class PlayerData {
 
-    private long steps;
+    private long steps = 0;
+
+    private Question activeQuestion = null;
+
+    private long questionExpireTime = 0;
+
+    private boolean receivedTutorialBook = false;
 
     public long getSteps() {
         return steps;
@@ -14,5 +22,53 @@ public class PlayerData {
 
     public void addSteps(long amount) {
         steps += amount;
+    }
+
+    public void resetSteps() {
+        steps = 0;
+    }
+
+    public Question getActiveQuestion() {
+        return activeQuestion;
+    }
+
+    public void setActiveQuestion(Question activeQuestion) {
+        this.activeQuestion = activeQuestion;
+    }
+
+    public boolean hasQuestion() {
+        return activeQuestion != null;
+    }
+
+    public void clearQuestion() {
+        activeQuestion = null;
+        questionExpireTime = 0;
+    }
+
+    public long getQuestionExpireTime() {
+        return questionExpireTime;
+    }
+
+    public void setQuestionExpireTime(long questionExpireTime) {
+        this.questionExpireTime = questionExpireTime;
+    }
+
+    public boolean hasReceivedTutorialBook() {
+        return receivedTutorialBook;
+    }
+
+    public void setReceivedTutorialBook(boolean receivedTutorialBook) {
+        this.receivedTutorialBook = receivedTutorialBook;
+    }
+
+    public long getNextEventSteps() {
+
+        long remainder = steps % 50;
+
+        if (remainder == 0) {
+            return 50;
+        }
+
+        return 50 - remainder;
     }
 }
