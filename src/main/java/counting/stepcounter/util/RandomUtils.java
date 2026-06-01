08be1bc13532713db377fcbf889c8f5834ba95cf
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 03:16:38 +0300

    Update RandomUtils.java

--- src/main/java/counting/stepcounter/util/RandomUtils.java
+++ src/main/java/counting/stepcounter/util/RandomUtils.java
@@ -1,1 +1,42 @@
-.
+package counting.stepcounter.util;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomUtils {
+
+    private static final Random RANDOM =
+            new Random();
+
+    public static int nextInt(
+            int bound
+    ) {
+        return RANDOM.nextInt(bound);
+    }
+
+    public static double nextDouble() {
+        return RANDOM.nextDouble();
+    }
+
+    public static boolean chance(
+            double chance
+    ) {
+        return RANDOM.nextDouble() < chance;
+    }
+
+    public static <T> T random(
+            List<T> list
+    ) {
+
+        if (list == null ||
+                list.isEmpty()) {
+            return null;
+        }
+
+        return list.get(
+                RANDOM.nextInt(
+                        list.size()
+                )
+        );
+    }
+}
