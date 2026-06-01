89cc0bfe6210ccf417d011968aeaeb6201e58f8f
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 19:43:38 +0300

    Update ModPackets.java

--- src/main/java/counting/stepcounter/network/ModPackets.java
+++ src/main/java/counting/stepcounter/network/ModPackets.java
@@ -1,12 +1,37 @@
 package counting.stepcounter.network;
 
 import counting.stepcounter.question.Question;
+import counting.stepcounter.question.QuestionManager;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.level.ServerPlayer;
 
 public class ModPackets {
 
     public static void register() {
 
+        PayloadTypeRegistry.playS2C().register(
+                OpenQuestionPayload.TYPE,
+                OpenQuestionPayload.CODEC
+        );
+
+        PayloadTypeRegistry.playC2S().register(
+                AnswerQuestionPayload.TYPE,
+                AnswerQuestionPayload.CODEC
+        );
+
+        ServerPlayNetworking.registerGlobalReceiver(
+                AnswerQuestionPayload.TYPE,
+                (payload, context) ->
+
+                        context.server().execute(() ->
+
+                                QuestionManager.answerQuestion(
+                                        context.player(),
+                                        payload.answer()
+                                )
+                        )
+        );
     }
 
     public static void sendQuestion(
@@ -15,9 +40,22 @@ public class ModPackets {
             long expireTime
     ) {
 
-        // TODO:
-        // Fabric networking implementation
-        // will be added later.
+        ServerPlayNetworking.send(
+                player,
 
+                new OpenQuestionPayload(
+
+                        question.getQuestion(),
+
+                        question.getAnswers().get(0),
+                        question.getAnswers().get(1),
+                        question.getAnswers().get(2),
+                        question.getAnswers().get(3),
+
+                        question.getCorrectIndex(),
+
+                        expireTime
+                )
+        );
     }
 }
