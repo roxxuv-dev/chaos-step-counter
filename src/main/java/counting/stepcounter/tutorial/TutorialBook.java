1eae5b930ad9b190bf37d2d9ec00800ea1a6fdc0
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 03:23:04 +0300

    Update TutorialBook.java

--- src/main/java/counting/stepcounter/tutorial/TutorialBook.java
+++ src/main/java/counting/stepcounter/tutorial/TutorialBook.java
@@ -3,47 +3,19 @@ package counting.stepcounter.tutorial;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.component.WrittenBookContent;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class TutorialBook {
 
-    public static void giveBook(ServerPlayer player) {
+    public static void giveBook(
+            ServerPlayer player
+    ) {
 
         ItemStack book =
-                new ItemStack(Items.WRITTEN_BOOK);
+                new ItemStack(
+                        Items.WRITABLE_BOOK
+                );
 
-        book.set(
-                net.minecraft.core.component.DataComponents.WRITTEN_BOOK_CONTENT,
-                new WrittenBookContent(
-                        Component.literal("Chaos Guide"),
-                        "Chaos Mod",
-                        0,
-                        List.of(
-                                Component.literal("""
-CHAOS STEP COUNTER
-
-Every 50 steps:
-A random event occurs.
-
-Sometimes:
-A question appears.
-
-Wrong answer:
-You die.
-
-Answer with:
-/answer <number>
-
-Good luck.
-""")
-                        ),
-                        true
-                )
-        );
-
-        player.getInventory().setItem(4, book);
+        player.getInventory()
+                .setItem(4, book);
     }
 }
