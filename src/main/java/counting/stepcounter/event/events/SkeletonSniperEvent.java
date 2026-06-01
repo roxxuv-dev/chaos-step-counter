78295d80b5818675fd8238aed82680dd0ca7ab3e
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:53:30 +0300

    Update SkeletonSniperEvent.java

--- src/main/java/counting/stepcounter/event/events/SkeletonSniperEvent.java
+++ src/main/java/counting/stepcounter/event/events/SkeletonSniperEvent.java
@@ -5,17 +5,24 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
-public class SkeletonSniperEvent implements ChaosEvent {
+public class SkeletonSniperEvent
+        implements ChaosEvent {
 
     @Override
     public String getName() {
+
         return "Skeleton Sniper";
     }
 
     @Override
-    public void execute(ServerPlayer player) {
+    public void execute(
+            ServerPlayer player
+    ) {
 
         ServerLevel level =
                 (ServerLevel) player.level();
@@ -32,12 +39,23 @@ public class SkeletonSniperEvent implements ChaosEvent {
 
         skeleton.setPos(
                 player.getX() + 10,
-                player.getY() + 2,
-                player.getZ()
+                player.getY() + 3,
+                player.getZ() + 10
         );
 
-        skeleton.setTarget(player);
+        skeleton.setItemSlot(
+                EquipmentSlot.MAINHAND,
+                new ItemStack(
+                        Items.BOW
+                )
+        );
 
-        level.addFreshEntity(skeleton);
+        skeleton.setTarget(
+                player
+        );
+
+        level.addFreshEntity(
+                skeleton
+        );
     }
 }
