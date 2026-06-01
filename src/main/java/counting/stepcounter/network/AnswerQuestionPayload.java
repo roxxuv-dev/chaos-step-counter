f0944e9dfd61f8be33058083e7dd441a69a01066
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 13:04:24 +0300

    Create AnswerQuestionPayload.java

---
+++ src/main/java/counting/stepcounter/network/AnswerQuestionPayload.java
@@ -0,0 +1,42 @@
+package counting.stepcounter.network;
+
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPayload;
+import net.minecraft.resources.ResourceLocation;
+
+public record AnswerQuestionPayload(
+        int answer
+) implements CustomPayload {
+
+    public static final Type<
+            AnswerQuestionPayload
+            > TYPE =
+            new Type<>(
+                    ResourceLocation.fromNamespaceAndPath(
+                            "step-counter",
+                            "answer_question"
+                    )
+            );
+
+    public static final StreamCodec<
+            RegistryFriendlyByteBuf,
+            AnswerQuestionPayload
+            > CODEC =
+            StreamCodec.of(
+                    (buf, payload) ->
+                            buf.writeInt(
+                                    payload.answer
+                            ),
+
+                    buf ->
+                            new AnswerQuestionPayload(
+                                    buf.readInt()
+                            )
+            );
+
+    @Override
+    public Type<? extends CustomPayload> type() {
+        return TYPE;
+    }
+}
