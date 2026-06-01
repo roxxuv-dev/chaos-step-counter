70dbfdb1b8e11acdd1f4dbd98dda37abf2205fa5
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:47:24 +0300

    Update TutorialManager.java

--- src/main/java/counting/stepcounter/tutorial/TutorialManager.java
+++ src/main/java/counting/stepcounter/tutorial/TutorialManager.java
@@ -3,6 +3,7 @@ package counting.stepcounter.tutorial;
 import counting.stepcounter.StepCounter;
 import counting.stepcounter.player.PlayerData;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.Items;
 
 public class TutorialManager {
 
@@ -14,6 +15,16 @@ public class TutorialManager {
                 StepCounter.getData(player);
 
         if (data.hasReceivedBook()) {
+            return;
+        }
+
+        if (player.getInventory()
+                .contains(
+                        Items.WRITTEN_BOOK
+                                .getDefaultInstance()
+                )) {
+
+            data.setReceivedBook(true);
             return;
         }
 
