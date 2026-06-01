dc74e92af5639404a326a002dd9921170a8c6619
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:31:55 +0300

    Update ClientPacketHandler.java

--- src/client/java/counting/stepcounter/client/network/ClientPacketHandler.java
+++ src/client/java/counting/stepcounter/client/network/ClientPacketHandler.java
@@ -1,9 +1,38 @@
 package counting.stepcounter.client.network;
 
+import counting.stepcounter.client.screen.QuestionScreen;
+import net.minecraft.client.Minecraft;
+
 public class ClientPacketHandler {
 
-    public static void register() {
+    public static void openQuestionScreen(
+            String question,
+            String a1,
+            String a2,
+            String a3,
+            String a4,
+            int correctAnswer,
+            long expireTime
+    ) {
 
-        // Future client packet registration
+        Minecraft client =
+                Minecraft.getInstance();
+
+        client.execute(() ->
+                client.setScreen(
+                        new QuestionScreen(
+                                question,
+                                a1,
+                                a2,
+                                a3,
+                                a4,
+                                correctAnswer,
+                                expireTime
+                        )
+                )
+        );
+    }
+
+    public static void register() {
     }
 }
