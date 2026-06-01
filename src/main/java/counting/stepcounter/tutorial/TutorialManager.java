c3f2ab9e660098dbda7f5a5f3c095d269c9ccb83
roxxuv-dev <rox.xuv@gmail.com>
2026-06-01 12:43:44 +0300

    Update TutorialManager.java

--- src/main/java/counting/stepcounter/tutorial/TutorialManager.java
+++ src/main/java/counting/stepcounter/tutorial/TutorialManager.java
@@ -1,1 +1,24 @@
-.
+package counting.stepcounter.tutorial;
+
+import counting.stepcounter.StepCounter;
+import counting.stepcounter.player.PlayerData;
+import net.minecraft.server.level.ServerPlayer;
+
+public class TutorialManager {
+
+    public static void handleJoin(
+            ServerPlayer player
+    ) {
+
+        PlayerData data =
+                StepCounter.getData(player);
+
+        if (data.hasReceivedBook()) {
+            return;
+        }
+
+        TutorialBook.giveBook(player);
+
+        data.setReceivedBook(true);
+    }
+}
